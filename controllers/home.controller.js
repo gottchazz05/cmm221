8c365fea420d416bfcd9e84ff02b4bb4c51757bb
gottchazz05 <32542049+gottchazz05@users.noreply.github.com>
2017-10-07 16:19:09 +0700

    Add content to twig

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,3 +1,29 @@
 exports.index = function(req,res) {
-	res.render('home.twig')
+
+	let content = {
+		highlight: {
+			url:'/assets/images/home/banner/highlight.jpg',
+			title: 'Hello'
+		},
+
+		thumbnail:[
+		{
+			url:'/assets/images/home/banner/thumbnail-1.jpg',
+			title: 'Market'
+		},
+		{
+			url:'/assets/images/home/banner/thumbnail-2.jpg',
+			title: 'Office'
+		},
+		{
+			url:'/assets/images/home/banner/thumbnail-3.jpg',
+			title: 'Cafe'
+		},
+		{
+			url:'/assets/images/home/banner/thumbnail-4.jpg',
+			title: 'Football'
+		}
+		]
+	}
+	res.render('home.twig', content)
 }
